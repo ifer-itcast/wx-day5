41c2fefb886ac1c475aa913030cf6199cb5ab516
ifer-itcast <yangkangkang@itcast.cn>
2021-04-04 10:11:46 +0800

    循环渲染Item项

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -11,7 +11,27 @@ Component({
    * 组件的初始数据
    */
   data: {
-    active: 0
+    active: 0,
+    "list": [
+      {
+        "pagePath": "pages/home/home",
+        "text": "首页",
+        "iconPath": "/images/tabs/home.png",
+        "selectedIconPath": "/images/tabs/home-active.png"
+      },
+      {
+        "pagePath": "pages/message/message",
+        "text": "消息",
+        "iconPath": "/images/tabs/message.png",
+        "selectedIconPath": "/images/tabs/message-active.png"
+      },
+      {
+        "pagePath": "pages/contact/contact",
+        "text": "联系我们",
+        "iconPath": "/images/tabs/contact.png",
+        "selectedIconPath": "/images/tabs/contact-active.png"
+      }
+    ]
   },
 
   /**
